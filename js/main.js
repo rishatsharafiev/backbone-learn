03907603896255a4dec8c72d8d861096b9202da4
rishatsharafiev <sharafiev.webmoney@gmail.com>
2015-02-07 00:40:21 +0300

    Did part2 of course

--- js/main.js
+++ js/main.js
@@ -1,17 +1,95 @@
-var AppView = Backbone.View.extend({
+var app = {};
 
-    el: $('#container'),
-
-    template: _.template("<h3>Hello <%= who %></h3>"),
-
-    initialize: function(){
-        this.render();
+// класс модели данных
+app.Todo = Backbone.Model.extend({
+    // поля по умолчанию при создании экземпляра
+    defaults: {
+        title: '',
+        completed: false
     },
 
-    render: function(){
-        this.$el.html(this.template({who: 'world!'}));
+    // валидация полей
+    validate: function(attrs) {
+        if(attrs.title.length > 25) {
+            return "запись должна быть не более 25 символов";
+        }
     }
 });
 
-var appView = new AppView();
+// класс коллекции данных, созданных по классу модели
+app.TodoList = Backbone.Collection.extend({
+    // класс модели
+    model: app.Todo,
 
+    // хранилище данных
+    localStorage: new Store("backbone-todo")
+
+});
+
+// класс отрисовки одной записи
+app.TodoView = Backbone.View.extend({
+    // элемент в котром будет новая запись
+    tagName: 'li',
+
+    // html шаблон
+    template: _.template($('#item-template').html()),
+
+    // отрисовка
+    render: function(){
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+
+});
+
+// экземпляр коллекции
+app.todoList = new app.TodoList();
+
+// класс отрисовки всех записей
+app.AppView = Backbone.View.extend({
+
+    // элемент в который помещают все записи
+    el: '#todoapp',
+
+    // инициализатор перед загрузкой
+    initialize: function () {
+        this.input = this.$('#new-todo'); // пое ввода
+        app.todoList.on('add', this.addOne, this); // обработчик события добавления в коллекцию записи
+        app.todoList.on('reset', this.addAll, this); // обработчик события обновления страницы
+        app.todoList.on("invalid", function(model, error) {  // обработчик события невалидной модели
+            alert(error);
+        });
+        app.todoList.fetch(); // подтянуть данные по умолчанию из хранилища
+    },
+
+    events: {
+        'keypress #new-todo': 'createTodoOnEnter' // обработчик события нажатия клавиши
+    },
+
+    createTodoOnEnter: function(e){
+        if ( e.which !== 13 || !this.input.val().trim() ) { // отследить enter
+            return;
+        }
+        app.todoList.create(this.newAttributes(), { validate: true }); // создать запись в коллекции
+        this.input.val(''); // отчистить поле ввода
+    },
+
+    addOne: function(todo){
+        var view = new app.TodoView({model: todo}); // создать элемент под одну запись
+        $('#todo-list').append(view.render().el); // добавить элемент в конец
+    },
+
+    addAll: function(){
+        this.$('#todo-list').html(''); // отчистить список напоминалок
+        app.todoList.each(this.addOne, this); // записать все напоминания
+    },
+
+    newAttributes: function(){
+        return {
+          title: this.input.val().trim(),
+          completed: false
+        }
+    }
+});
+
+app.appView = new app.AppView();
