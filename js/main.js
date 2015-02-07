a4dfdafbb78c1fabe623afdf7e5eefe19d2a841a
rishatsharafiev <sharafiev.webmoney@gmail.com>
2015-02-08 01:55:41 +0300

    Did part3 of course

--- js/main.js
+++ js/main.js
@@ -37,7 +37,44 @@ app.TodoView = Backbone.View.extend({
     // отрисовка
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
+        this.input = this.$('.edit');
         return this;
+    },
+
+    initialize: function(){
+        this.model.on('change', this.render, this);
+        this.model.on('destroy', this.remove, this);
+    },
+
+    events: {
+        'dblclick label' : 'edit',
+        'keypress .edit' : 'updateOnEnter',
+        'blur .edit' : 'close',
+        'click .toggle': 'toggleCompleted',
+        'click .destroy': 'destroy'
+    },
+
+    edit: function(){
+        this.$el.addClass('editing');
+        this.input.focus();
+    },
+
+    close: function(){
+        var value = this.input.val().trim();
+        if(value) {
+            this.model.save({title: value});
+        }
+        this.$el.removeClass('editing');
+    },
+
+    updateOnEnter: function(e){
+        if(e.which == 13){
+            this.close();
+        }
+    },
+
+    destroy: function(){
+        this.model.destroy();
     }
 
 });
